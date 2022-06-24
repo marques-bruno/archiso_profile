7128d29f66d1624a48a259ebb0553e8c0f2da871
Bruno <bmarques68@gmail.com>
2022-06-24 21:44:40 +0200

    Many Fixes...

--- airootfs/root/copy_me_in_user_home/run_playbook.sh
+++ airootfs/root/copy_me_in_user_home/run_playbook.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-echo "If you run into issues Make sure packages ansible and git are installed"
+
+pacman -Sy ansible git ansible-galaxy python-pip
+pip install packaging
+ansible-galaxy collection install kewlfft.aur
 
 git clone --recurse-submodules https://github.com/marques-bruno/ansible-arch-setup.git .ansible
 ansible-playbook -K .ansible/roles/main.yml
